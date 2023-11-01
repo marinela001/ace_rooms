41f4104d7d246ad1fd0c3a3034bfcd00651c4b03
Ubuntu <ubuntu@ip-172-31-19-158.eu-central-1.compute.internal>
2023-11-01 22:49:20 +0000

    changes

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+   apps: [
+	       {
+		             script: "npm start",
+		           },
+	     ],
+
+  deploy : {
+    production : {
+	    key:"test.pem",
+      user : 'ubuntu',
+      host : '3.72.85.69',
+      ref  : 'origin/master',
+      repo : 'git@github.com:marinela001/ace_rooms.git',
+      path : '/home/ubuntu/ace_rooms',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
