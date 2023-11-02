e0b050772aff7aeffbe3643fc1fb98526114dc08
unknown <nelaaga@icloud.com>
2023-11-02 12:21:18 +0100

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       host : '3.72.85.69',
       ref  : 'origin/main',
       repo : 'git@github.com:marinela001/ace_rooms.git',
-      path : '/home/ubuntu/ace_rooms',
+      path : '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
